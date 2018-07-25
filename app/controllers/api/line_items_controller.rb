dc09d0a1f081d7683dc5856c451ca8aa9fc54f3e
Tilingo <nestirco@gmail.com>
2018-07-25 17:08:15 -0400

    Dependent destroy added to order and product models

--- app/controllers/api/line_items_controller.rb
+++ app/controllers/api/line_items_controller.rb
@@ -5,4 +5,33 @@ class Api::LineItemsController < ApplicationController
         render json: @items
     end
 
+    def create
+        product = Product.find(params[:product_id])
+        order_id = params[:order_id]
+
+        @item = product.line_items.create
+        @item.update!(name: product.name, price: product.price, qty: 1, order_id: order_id)
+
+        render json: @item
+    end
+
+    def update
+        @item = Order.find(params[:order_id]).line_items.find(params[:id])
+        @item.update!(item_params)
+
+        render json: @item
+    end
+
+    def destroy
+        @item = Order.find(params[:order_id]).line_items.find(params[:id])
+        @item.destroy
+
+        render status: :ok
+    end
+
+    private
+    def item_params
+        params.require(:line_item).permit(:qty)
+    end
+
 end
